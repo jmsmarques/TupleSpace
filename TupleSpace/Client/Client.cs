d4ae8deffbf801c35ee0b3bb055bc9b67c93cd33
miguelgaoreis <mikelreis@hotmail.com>
2018-11-07 16:55:56 +0000

    Update TupleSpace/Client/Client.cs
    Fix input

--- TupleSpace/Client/Client.cs
+++ TupleSpace/Client/Client.cs
@@ -34,47 +34,54 @@ namespace Client
             int loop = 1;
             string input;
             bool go = true;
+            string line;
             while (go)
             {                
                 input = Console.ReadLine();
-                string[] words = input.Split(' ');
-                switch (words[0])
+                System.IO.StreamReader file = new System.IO.StreamReader(input);
+                while ((line = file.ReadLine()) != null)
                 {
-                    case "add":
-                        for(int i = 0; i < loop; i++)
-                        {
-                            client.Add(words[1]);
-                        }                        
-                        break;
-                    case "read":
-                        for (int i = 0; i < loop; i++)
-                        {
-                            client.Read(words[1]);
-                        }
-                        break;
-                    case "take":
-                        for (int i = 0; i < loop; i++)
-                        {
-                            client.Take(words[1]);
-                        }
-                        break;
-                    case "wait":
-                        client.Wait = System.Convert.ToInt32(words[1]);
-                        break;
-                    case "begin-repeat":
-                        loop = System.Convert.ToInt32(words[1]);
-                        break;
-                    case "end-repeat":
-                        loop = 1;
-                        break;
-                    case "exit":
-                    case "Exit":
-                        go = false;
-                        break;
-                    default:
-                        Console.WriteLine("Command not recognized.\n");
-                        break;
+                    System.Console.WriteLine(line);
+                    string[] words = line.Split(' ');
+                    switch (words[0])
+                    {
+                        case "add":
+                            for (int i = 0; i < loop; i++)
+                            {
+                                client.Add(words[1]);
+                            }
+                            break;
+                        case "read":
+                            for (int i = 0; i < loop; i++)
+                            {
+                                client.Read(words[1]);
+                            }
+                            break;
+                        case "take":
+                            for (int i = 0; i < loop; i++)
+                            {
+                                client.Take(words[1]);
+                            }
+                            break;
+                        case "wait":
+                            client.Wait = System.Convert.ToInt32(words[1]);
+                            break;
+                        case "begin-repeat":
+                            loop = System.Convert.ToInt32(words[1]);
+                            break;
+                        case "end-repeat":
+                            loop = 1;
+                            break;
+                        case "exit":
+                        case "Exit":
+                            go = false;
+                            break;
+                        default:
+                            Console.WriteLine("Command not recognized.\n");
+                            break;
+                    }
                 }
+                
             }
         }
     }
